e8425c55311d67582f3eb2e986694caff9537751
rhetoricalraj <rajpsheth@gmail.com>
2019-05-25 15:03:15 +0530

    added comments

--- listFollowers.py
+++ listFollowers.py
@@ -1,13 +1,18 @@
 from bs4 import BeautifulSoup
 
+# HTML file which you have saved from https://www.instagram.com/<your_username>/followers/
+# You must log in, on your browser
 fileName = 'Insta-followers-25-05-2019.html'
 
+
 curDate = "26-05-2019"
+# All your followers list will be saved in ./data/followers-<curData> file
 outputFileName = "data/followers-"+curDate
 
 followers_file = open(outputFileName,"w+")
 
 with open(fileName) as fp:
+    # Created Beautiful Soup object which parses HTML file
     soup = BeautifulSoup(fp)
 
 # Check the class name by clicking on inspect element.
@@ -19,15 +24,21 @@ with open(fileName) as fp:
 # You must sign in on your PC
 
 className = "FPmhX notranslate _0imsa "
+
 # Here we are searching of all <a> elements with class name as className
 # bs4 can do this beautifully and it stores in resultSet
 
+# resultSet = <bs4-Object>("<tag-name>", [params..])
 resultSet = soup.find_all("a", class_ = className)
 
+# For loop iterates all users who are following you
 for x in resultSet:
+    # Uncomment following line to debug
+    # print(x)
+    # x.text contains user name who follows you
     followers_file.write(x.text+'\n')
 
-# all your current followers will be stored in a file.
+# All your current followers will be stored in a file.
 # after few days you can once again run the script find out missing names in the new file
 # which were there earlier.
 
