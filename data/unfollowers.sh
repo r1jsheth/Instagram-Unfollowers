8c40cabb293a999903b5a5f52b63116cf502a4b1
rhetoricalraj <rajpsheth@gmail.com>
2019-05-25 14:54:38 +0530

    add comments

--- data/unfollowers.sh
+++ data/unfollowers.sh
@@ -3,5 +3,11 @@ echo "Enter previous filename:"
 read prevFileName
 echo "Enter new filename:"
 read newFileName
+
+# Following line of code finds followers gained
+# Output stored in newFollowers file, change according to your need
 fgrep -vxf $prevFileName $newFileName >> newFollowers
+
+# Following line of code finds all the followers lost
+# Output stored in unfollowers file, change according to your need
 fgrep -vxf $newFileName $prevFileName >> unfollowers
