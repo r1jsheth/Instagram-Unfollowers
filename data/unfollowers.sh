42a063ee5498ef4409cb9dc6ac0226191c80128d
Raj Sheth <raj@saveo.in>
2019-06-02 11:59:17 +0530

    updated unfollowers.sh

--- data/unfollowers.sh
+++ data/unfollowers.sh
@@ -7,7 +7,11 @@ read newFileName
 # Following line of code finds followers gained
 # Output stored in newFollowers file, change according to your need
 fgrep -vxf $prevFileName $newFileName >> newFollowers
+echo "New Followers:"
+cat newFollowers
 
 # Following line of code finds all the followers lost
 # Output stored in unfollowers file, change according to your need
 fgrep -vxf $newFileName $prevFileName >> unfollowers
+echo "Unfollowers:"
+cat unfollowers
